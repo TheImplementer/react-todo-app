a95b4011c4c1e2c8fddff4d42ac3881732a1316d
Enrico Pelizzon <enrico.pelizzon@gmail.com>
2019-05-09 08:26:01 +0100

    Generating id for new entries

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
+import uuid from 'uuid/v4';
 import AddNewToDo from './AddNewToDo';
 import ToDo from './ToDo';
 
+interface ToDoEntry {
+  id: string;
+  description: string;
+}
+
 const ToDoList: React.FC = () => {
-  const [entries, setEntries] = useState<string[]>([]);
+  const [entries, setEntries] = useState<ToDoEntry[]>([]);
 
   function handleNewEntry(newEntry: string) {
-    setEntries(currentEntries => currentEntries.concat(newEntry));
+    setEntries(currentEntries =>
+      currentEntries.concat({
+        id: uuid(),
+        description: newEntry,
+      }),
+    );
   }
 
   return (
     <div>
       <AddNewToDo onNewEntry={handleNewEntry} />
       {entries.map((entry, idx) => (
-        <ToDo description={entry} key={idx} />
+        <ToDo description={entry.description} key={entry.id} />
       ))}
     </div>
   );
